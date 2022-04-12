d15413cc857d4e388fd33757265a3108a38da863
Blessy Antony <blessyantony@vt.edu>
2022-04-12 03:55:22 +0000

    Build Commit

---
+++ src/models/logistic_regression.py
@@ -0,0 +1,7 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
+
+
+class LogisticRegression:
+    def __init__(self, ):
